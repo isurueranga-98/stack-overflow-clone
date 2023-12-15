dd99e1e94dcb4a72fe61b4d78ae67958d5d48343
Isuru Eranga <isurueranga.0@gmail.com>
2023-12-15 22:05:56 +0530

    slidebar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -11,15 +11,19 @@ const ThemeContext =  createContext <ThemeContextType | undefined> (undefined);
 
 export function ThemeProvider ({children} : {children: React.ReactNode}){
     const [mode, setMode] = useState('');
-    const handleThemeChange = () => {
+    const handleThemeChange = () => 
+    {
 
-        if(mode === 'dark'){
-            setMode('light');
-            document.documentElement.classList.add('light');
-        }else{
+        if(localStorage.theme === 'dark' || (!("theme" in localStorage) && window.matchMedia("(prefers-color-schema: dark)").matches)){
             setMode('dark');
-            document.documentElement.classList.add('dark')
+            
+            document.documentElement.classList.add('dark');
+        }else{
+            setMode('light');
+            
+            document.documentElement.classList.remove('dark')
         }
+
     }
 
     useEffect (() => {
